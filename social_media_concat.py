b942043993265ced9abfbeb26920f32745aad363
icZsh <58492547+icZsh@users.noreply.github.com>
2020-09-21 13:34:44 +0800

    Concat social media data and push to database

--- social_media_concat.py
+++ social_media_concat.py
@@ -16,18 +16,23 @@ class Database(object):
         )
 
     def get_wb_article(self):
-        sql1 = "SELECT * FROM media.ods_weibo_article"
-        weibo = pd.read_sql(sql1, self.conn)
+        sql = "SELECT * FROM media.ods_weibo_article"
+        weibo = pd.read_sql(sql, self.conn)
         return weibo
 
+    def get_wb_color(self):
+        sql = "SELECT * FROM media.ods_weibo_color_info"
+        wb_color = pd.read_sql(sql, self.conn)
+        return wb_color
+
     def get_wc_article(self):
-        sql2 = "SELECT * FROM wechat.wechat_article_list"
-        wechat_article_list = pd.read_sql(sql2, self.conn)
+        sql = "SELECT * FROM wechat.wechat_article_list"
+        wechat_article_list = pd.read_sql(sql, self.conn)
         return wechat_article_list
 
     def get_wc_article_dynmaic(self):
-        sql3 = "SELECT * FROM wechat.wechat_article_dynamic"
-        wechat_article_dynamic = pd.read_sql(sql3, self.conn)
+        sql = "SELECT * FROM wechat.wechat_article_dynamic"
+        wechat_article_dynamic = pd.read_sql(sql, self.conn)
         return wechat_article_dynamic
 
     def close_database(self):
@@ -49,10 +54,11 @@ brand_list = {'MzA4MDE5MjkxNA==': 'Clinique',
 
 class Concat(object):
 
-    def __init__(self, wc_article, wc_dynamic, weibo):
+    def __init__(self, wc_article, wc_dynamic, weibo, wb_color):
         self.wc_article = wc_article
         self.wc_dynamic = wc_dynamic
         self.weibo = weibo
+        self.wb_color = wb_color
 
     def clean_wc(self):
         df_wc = pd.merge(self.wc_dynamic, self.wc_article, on=['__biz', 'sn'], how='left')
@@ -69,6 +75,12 @@ class Concat(object):
                                               'title': 'text'})
         return wechat_df
 
+    def clean_wb_color(self):
+        wb_color = self.wb_color
+        wb_color = wb_color.drop(['gmt_create', 'gmt_modified'], axis=1).rename(columns={'url': 'cover',
+                                                                                         'article_id': 'sn'})
+        return wb_color
+
     def clean_wb(self):
         df_wb = self.weibo[
             ['name', 'article_id', 'text', 'pics', 'created_date', 'attitudes_count', 'comments_count',
@@ -84,13 +96,16 @@ class Concat(object):
                                             'comments_count': 'comment',
                                             'reposts_count': 'repost',
                                             'pics': 'cover'})
-        return weibo_df
+        wb_df = pd.merge(weibo_df, self.clean_wb_color(),
+                         how='left',
+                         on=['sn', 'cover', 'platform']).drop(['id', 'hex_color_express'], axis=1)
+        return wb_df
 
     def concat_df(self):
         df = pd.concat([self.clean_wc(), self.clean_wb()], ignore_index=True)
         df = df[
             ['platform', 'account', 'publish_time', 'sn', 'read_num', 'read_like', 'comment', 'repost', 'text',
-             'cover']]
+             'cover', 'color']]
         return df
 
 
@@ -108,14 +123,16 @@ class PushDatabase(object):
     def push_to_db(self):
         dbutils = DBUtils(self.config)
         db = "media"
-        dbutils.push_table_df(df=self.df, db=db, table="wb_wc_article_list_test", if_exists="append")
+        dbutils.push_table_df(df=self.df, db=db, table="wb_wc_article_list_test", if_exists="replace")
 
 
 if __name__ == '__main__':
     wechat_article_list = Database().get_wc_article()
     wechat_article_dynamic = Database().get_wc_article_dynmaic()
     weibo = Database().get_wb_article()
+    wb_color = Database().get_wb_color()
     Database().close_database()
-    test = Concat(wechat_article_list, wechat_article_dynamic, weibo)
+    test = Concat(wechat_article_list, wechat_article_dynamic, weibo, wb_color)
     concat_result = test.concat_df()
+    concat_result['color'] = concat_result['color'].fillna(value='unknown')
     PushDatabase(concat_result).push_to_db()
